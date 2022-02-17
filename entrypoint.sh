ee0d2376865adcfb278c1709c8cb888badaf2fe7
martin <martin.buehrer@gyrotoools.com>
2022-02-17 10:33:08 +0100

    changed names of environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,19 @@ if [ -f "/pgp/key.pgp" ]; then
     echo "pgp key file found."    
     gpg -v --batch --import /pgp/key.pgp
 
-elif [[ ! -z "${SETT_PGP_KEY}" ]]; then
-  echo "SETT_PGP_KEY found."
+elif [[ ! -z "${PGP_KEY}" ]]; then
+  echo "PGP_KEY found."
   
-  if [[ -z "${SETT_PGP_KEY_PWD}" ]]; then
-    echo "ERROR: environment variable SETT_PGP_KEY_PWD not found." 
+  if [[ -z "${PGP_KEY_PWD}" ]]; then
+    echo "ERROR: environment variable PGP_KEY_PWD not found." 
     exit 1 
   else
-    echo "SETT_PGP_KEY_PWD found."
+    echo "PGP_KEY_PWD found."
     touch /pgp/key.pw
-    printf "%s" "$SETT_PGP_KEY_PWD" > "/pgp/key.pw"
+    printf "%s" "$PGP_KEY_PWD" > "/pgp/key.pw"
   fi
 
-  gpg -v --batch --import <(echo "$SETT_PGP_KEY")  
+  gpg -v --batch --import <(echo "$PGP_KEY")  
 
 else
   echo "ERROR: no file or environment variable with th pgp key found" 
